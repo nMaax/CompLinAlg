69c9c285a0d751ed4ec061468d3c419f7ab0af2f
nMaax <carli.massimiliano01@gmail.com>
2024-12-22 13:55:08 +0100

    correction pseudo-code x1 and x2 definition

--- HW2/shifting_deflation.m
+++ HW2/shifting_deflation.m
@@ -1,7 +1,7 @@
-function [V,D] = shiftingDeflation(A, lambda, x, M)
+function [V,D] = shiftingDeflation(A, lambda1, x1, M)
     
     n = size(x, 1);
-    xpeye1 = x(1) + eye(1, n);
+    xpeye1 = x1 + eye(1, n);
     P1 = eye(n) - 2 * (xpeye1*xpeye1') / norm(xpeye1)^2; % norm = 2(1 + eye * x)
     
     % P1 =  P1' = inv(P1) => P1'P1 = P1P1 = I_n
@@ -10,7 +10,7 @@ function [V,D] = shiftingDeflation(A, lambda, x, M)
     A2 = B1(2:end, 2:end);
 
     
-    xpeye2 = x(2) + eye(1, n-1);
+    xpeye2 = x2 + eye(1, n-1);
     P1 = eye(n) - 2 * (xpeye2*xpeye2') / norm(xpeye2)^2; % norm = 2(1 + eye * x)
     
     % P2 =  P2' = inv(P2) => P2'P2 = P2P2 = I_n-1
