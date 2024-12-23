38ef079312ba9adda72ed40781aee88ff76806b8
nMaax <carli.massimiliano01@gmail.com>
2024-12-23 14:26:54 +0100

    deleted unused files

--- HW2/DeflationProcess.m
+++
@@ -1,9 +0,0 @@
-function [deflated_matrix] = DeflationProcess(A, x)
-    [m,n] = size(A);
-    if n~=m
-        error('Not square matrix')
-    end
-    P = householder(x);
-    B = P*A*P;
-    deflated_matrix = B(2:n,2:n);
-end
