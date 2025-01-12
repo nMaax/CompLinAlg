f0cba65a9e2b07697f2e3307c9bcd79e19cb34af
nMaax <carli.massimiliano01@gmail.com>
2025-01-12 19:49:54 +0100

    simplified variables names and code structure in bidiagonalize.py and householder.py

--- HW3/bidiagonalize.py
+++ HW3/bidiagonalize.py
@@ -15,18 +15,18 @@ def bidiagonalize(A):
 
         #k = i
 
-        a = B[k:, k]
-        P_transformation = np.eye(m)
-        P_transformation[k:, k:] = householder(a)
-        B = P_transformation @ B
-        P = P_transformation @ P
+        a = B[k:m, k]
+        P_tilde = np.eye(m)
+        P_tilde[k:m, k:m] = householder(a)
+        B = P_tilde @ B
+        P = P_tilde @ P
         
         if k <= n-2:
-            b = B[k, k+1:]
-            H_transformation = np.eye(n)
-            H_transformation[k+1:, k+1:] = householder(b)
-            B = B @ H_transformation
-            H = H @ H_transformation
+            b = B[k, k+1:n]
+            H_tilde = np.eye(n)
+            H_tilde[k+1:n, k+1:n] = householder(b)
+            B = B @ H_tilde
+            H = H @ H_tilde
 
     return B, P, H
 
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     
     # Test the bidiagonalize function with a sample matrix
     A = np.array([[4, 1, 3], [2, 6, 5], [1, 2, 3], [5, 4, 2]])
-    print("Original matrix A:")
+    print("\nOriginal matrix A:")
     print(A)
 
     B, P, H = bidiagonalize(A)
@@ -42,8 +42,5 @@ if __name__ == "__main__":
     print("\nBidiagonalized matrix B:")
     print(np.round(B, decimals=8))
 
-    # Verify the result
-    print("\nVerification (P @ A @ H):")
-    print(np.round(P @ A @ H, decimals=8))
-
     print("\nP @ A @ H == B? ", np.allclose(P @ A @ H, B, atol=1e-8))
+    print()
--- HW3/householder.py
+++ HW3/householder.py
@@ -2,8 +2,11 @@ import numpy as np
 
 def householder(x):
     v = x.reshape(x.size, 1)
+
     sigma = np.sign(v[0, 0]) * np.linalg.norm(v)
     u = v + sigma * np.eye(v.size, 1)
-    u = u / np.linalg.norm(u) 
+    u = u / np.linalg.norm(u)
+    
     Px = np.identity(u.size) - 2 * u @ u.T
+    
     return Px
